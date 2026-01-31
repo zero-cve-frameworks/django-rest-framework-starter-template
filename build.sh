426fdf5353ef329c1eed49a84a3e26c53c0f9d55
Himel Das <60639301+thehimel@users.noreply.github.com>
2026-01-31 17:49:08 +0100

    chore: remove `build.sh` and related Vercel configuration

--- build.sh
+++
@@ -1,9 +0,0 @@
-# This script is used for deploying the application on Vercel.
-# Note: Dependencies are automatically installed by Vercel from requirements.txt
-
-echo "Migrating Database..."
-python manage.py makemigrations
-python manage.py migrate
-
-echo "Collecting static files."
-python manage.py collectstatic --no-input
