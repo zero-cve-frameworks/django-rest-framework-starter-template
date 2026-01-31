73455094d4b738e67155a038e3d82d275325d93b
Himel Das <60639301+thehimel@users.noreply.github.com>
2026-01-31 17:47:03 +0100

    chore: simplify `build.sh` for Vercel deployment by removing redundant pip commands

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 # This script is used for deploying the application on Vercel.
-
-echo "Upgrading pip..."
-python3 -m pip install --upgrade pip setuptools
-
-echo "Installing packages..."
-pip3 install -r requirements.txt
+# Note: Dependencies are automatically installed by Vercel from requirements.txt
 
 echo "Migrating Database..."
-python3 manage.py makemigrations
-python3 manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 
 echo "Collecting static files."
-python3 manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
